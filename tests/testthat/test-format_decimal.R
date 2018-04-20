dc4901c39dd1689fda136ad1cbc40b316ea5414d
David Holstius <david.holstius@gmail.com>
2018-04-20 10:51:30 -0700

    clean t0182 out of test

--- tests/testthat/test-format_decimal.R
+++ tests/testthat/test-format_decimal.R
@@ -8,15 +8,6 @@ test_that("NAs", {
 
 is_int <- function (x) (x == round(x))
 
-t0182_q2 <-
-  here::here("tests", "testthat", "t0182_q2.Rds") %>%
-  readRDS()
-
-# x <- tail(keep(t0182_q2, Negate(is_int)), n = 3)
-# test_that("x", {
-#   expect_equal(x, c(52.5999984741211, 417.399993896484, 103.800003051758), tol = 1e-12)
-# })
-
 x <- c(52.5999984741211, 417.399993896484, 103.800003051758)
 
 test_that("guess_digits", {
