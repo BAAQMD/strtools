1e47c58bc5e0e4e6aab26e3aac38804028458a22
David Holstius <david.holstius@gmail.com>
2018-05-03 12:17:33 -0700

    moved from package SIP2016 to here

---
+++ R/format_EIC.R
@@ -0,0 +1,11 @@
+#' format_EIC
+#'
+#' @note FIXME: make this available to `../data-raw/SIP2016_crosswalk.R`
+#'
+#' @export
+format_EIC <- function (x) {
+  is_only_digits <- function (...) str_detect(..., "^[[:digit:]]+$")
+  d <- if_else(is_only_digits(x), str_pad(x, 14, "left", "0"), NA_character_)
+  str_c(str_sub(d, 1, 3), str_sub(d, 4, 6), str_sub(d, 7, 10), str_sub(d, 11, 14), sep = "-")
+}
+
