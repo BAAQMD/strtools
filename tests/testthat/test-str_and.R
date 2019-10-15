e831dd84f7055fa819b4d675aae7df8d81c17c17
David Holstius <david.holstius@gmail.com>
2019-10-15 13:39:55 -0700

    add str_and()

---
+++ tests/testthat/test-str_and.R
@@ -0,0 +1,21 @@
+context("str_and")
+
+test_that("zero items", {
+  expect_equal(str_and(NULL), NULL)
+  expect_equal(str_and(character(0)), character(0))
+})
+
+test_that("one item", {
+  x <- c("apples")
+  expect_equal(str_and(x), "apples")
+})
+
+test_that("two items", {
+  x <- c("apples", "oranges")
+  expect_equal(str_and(x), "apples and oranges")
+})
+
+test_that("three items", {
+  x <- c("apples", "oranges", "pears")
+  expect_equal(str_and(x), "apples, oranges, and pears")
+})
