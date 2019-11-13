dba0c67ed6e3a8c1f5b51d28fff03b2d2d65ec46
David Holstius <david.holstius@gmail.com>
2019-11-13 09:56:57 -0800

    add format_percent_change()

---
+++ R/format_percent_change.R
@@ -0,0 +1,35 @@
+#' format_percent_change
+#'
+#' Format a relative change.
+#'
+#' @param x numeric
+#' @param na value to use if `x` is `NA`; defaults to an empty string
+#' @param zero value to use if `x` is unity; defaults to "0%" (unsigned, no digits).
+#'
+#' @export
+format_percent_change <- function (
+  x,
+  digits = 0,
+  na = "",
+  zero = "0%"
+) {
+
+  change <- (x - 1)
+
+  formatted <-
+    if_else(
+      change == 0,
+      true = zero,
+      false = format_percentage(abs(change), digits = digits),
+      missing = na)
+
+  negative <- which(change < 0)
+  formatted[negative] <- str_c("-", formatted[negative])
+
+  positive <- which(change > 0)
+  formatted[positive] <- str_c("+", formatted[positive])
+
+  return(formatted)
+
+}
+
