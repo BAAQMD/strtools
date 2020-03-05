7e670a1a9c89a17dc553c03b368484aeb822479d
David Holstius <david.holstius@gmail.com>
2020-03-05 14:50:01 -0800

    enhance format_SI() with fixed = TRUE

--- R/format_SI.R
+++ R/format_SI.R
@@ -7,18 +7,29 @@
 #' format_SI(1)
 #' format_SI(9:11)
 #' format_SI(c(33, 333, 3333, 33333333))
+#' format_SI(seq(0, 1.2, len = 7) * 1000)
+#' format_SI(seq(0, 1.2, len = 7) * 1000, fixed = TRUE)
 #'
 #' @seealso (Stack Overflow post)[http://stackoverflow.com/questions/21045545/how-to-accurately-display-si-prefix-for-numbers-in-y-axis-scale-of-plot-made-wit]
 #'
 #' @export
-format_SI <- function (x, ...) {
+format_SI <- function (
+  x,
+  fixed = FALSE,
+  ...
+) {
 
   breaks <- 10 ** seq(-24, 24, by = 3)
   prefixes <- c("y", "z", "a", "f", "p", "n", "µ", "m", " ",
                 "k", "M", "G", "T", "P", "E", "Z", "Y")
 
-  # Vector with array indices according to position in intervals
-  i <- findInterval(abs(x), breaks)
+  if (isTRUE(fixed)) {
+    # Single array index corresponding to largest value of x
+    i <- findInterval(abs(max(x)), breaks)
+  } else {
+    # Vector with array indices according to position in intervals
+    i <- findInterval(abs(x), breaks)
+  }
 
   # Set prefix to " " for very small values < 1e-24
   i <- ifelse(i == 0, which(breaks == 1e0), i)
--- tests/testthat/test-format_SI.R
+++ tests/testthat/test-format_SI.R
@@ -13,4 +13,20 @@ test_that("dynamic range", {
     format_SI(c(33, 333, 3333, 33333333)),
     c("33.0", "333.0", "3.3k", "33.3M"))
 
+  expect_identical(
+    format_SI(seq(0, 1.2, length.out = 7) * 1000),
+    c("0.0", "200.0", "400.0", "600.0", "800.0", "1000.0", "1.2k"))
+
+  expect_identical(
+    format_SI(seq(0, 1.2, length.out = 7) * 1000),
+    format_SI(seq(0, 1.2, length.out = 7) * 1000, fixed = FALSE))
+
+})
+
+test_that("fixed range", {
+
+  expect_identical(
+    format_SI(seq(0, 1.2, length.out = 7) * 1000, fixed = TRUE),
+    c("0.0k", "0.2k", "0.4k", "0.6k", "0.8k", "1.0k", "1.2k"))
+
 })
