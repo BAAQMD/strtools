8655228c2dcaf2a077a70b1fee3c806a75687e8f
David Holstius <david.holstius@gmail.com>
2022-03-11 16:29:17 -0800

    support str_tex(..., output = ...)

--- R/str_tex.R
+++ R/str_tex.R
@@ -3,6 +3,7 @@
 #' Glues its `...` argument(s) together, then runs the result through [latex2exp::TeX()].
 #'
 #' @param ... (character) combined via [stringr::str_glue(...)].
+#' @param output (character) one of "expression", "text", or "ast"; see [latex2exp::TeX()].
 #'
 #' @importFrom stringr str_glue
 #' @importFrom latex2exp TeX
@@ -11,8 +12,9 @@
 #' @export
 #'
 #' @examples
-str_tex <- function (...) {
+str_tex <- function (..., output = c("expression", "text", "ast")) {
+  output <- match.arg(output)
   glued <- stringr::str_glue(...)
-  tex <- latex2exp::TeX(as.character(glued))
+  tex <- latex2exp::TeX(as.character(glued), output = output)
   return(tex)
 }
