faaa4e5cf0e508296fcd06c4680ae603f0aff0e7
David Holstius <david.holstius@gmail.com>
2022-03-11 16:23:35 -0800

    support str_ugm3(format="character")

--- R/str_ugm3.R
+++ R/str_ugm3.R
@@ -1,37 +1,45 @@
 #' str_ugm3
 #'
-#' @return
-#' @export
+#' @param format (character) one of "character", "utf8", "html", or "TeX"
+#' @param verbose (logical)
 #'
+#' @return representation of "ug/m3" (micrograms per cubic meter)
+#'
+#' @export
 str_ugm3 <- function (
-  format = c("utf8", "html", "TeX"),
+  format = c("character", "utf8", "html", "TeX"),
   verbose = getOption("verbose", default = FALSE)
 ) {
-
-  msg <- function (...) if(isTRUE(verbose)) message("[str_ugm3] ", ...)
-
+  msg <- function(...) if (isTRUE(verbose))
+    message("[str_ugm3] ", ...)
   if (missing(format)) {
     if (isTRUE(knitr::is_html_output())) {
       format <- "html"
-    } else if (isTRUE(knitr::is_latex_output())) {
+    }
+    else if (isTRUE(knitr::is_latex_output())) {
       format <- "TeX"
-    } else {
+    }
+    else {
       format <- match.arg(format)
     }
-  } else {
+  }
+  else {
     format <- match.arg(format)
   }
-
-  if (format == "utf8") {
+  if (format == "character") {
+    token <- "ug/m3"
+  }
+  else if (format == "utf8") {
     token <- "µg/m³"
-  } else if (format == "html") {
+  }
+  else if (format == "html") {
     token <- "µg/m<sup>3</sup>"
-  } else if (format == "TeX") {
+  }
+  else if (format == "TeX") {
     token <- "µg/m$^3$"
-  } else {
+  }
+  else {
     stop("Unknown format ", format)
   }
-
   return(token)
-
 }
