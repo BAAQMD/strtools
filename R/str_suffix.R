1d6f45918346582bf9c369070b9dcbab752abee4
David Holstius <david.holstius@gmail.com>
2022-03-11 16:25:28 -0800

    add str_suffix.R

---
+++ R/str_suffix.R
@@ -0,0 +1,19 @@
+#' str_suffix
+#'
+#' @param x (character) to be suffixed
+#' @param suffix (character) length one, or will be recycled
+#' @param list (optional) indices of elements to suffix; defaults to all
+#'
+#' @return (character)
+#'
+#' @export
+#'
+#' @examples
+#' str_suffix(1:2, "m")
+#' str_suffix(1:5, " or more", 3)
+#' str_suffix(1:3, c("st", "nd", "rd"))
+str_suffix <- function (x, suffix, list = seq_along(x)) {
+  suffixed <- paste0(x, suffix)
+  result <- replace(x, list, suffixed[list])
+  return(result)
+}
