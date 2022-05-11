9b44d259371597edcf416b6ed321017e4c624ead
David Holstius <david.holstius@gmail.com>
2022-05-11 12:05:14 -0700

    namespace units::drop_units()

--- R/format_ugm3.R
+++ R/format_ugm3.R
@@ -3,13 +3,14 @@
 #' @param x numeric
 #' @param digits passed to [format_digits()]
 #'
-#' @return
+#' @importFrom units drop_units
+#'
 #' @export
 #'
 format_ugm3 <- function (
   x,
   digits = 3
 ) {
-  format_digits(drop_units(x), digits = digits)
   x <- set_units(x, "ug/m^3")
+  format_digits(units::drop_units(x), digits = digits)
 }
